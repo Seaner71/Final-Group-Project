41577f1b40d8e135036c64a0fc58cef5645b98b7
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-12-05 15:03:38 -0500

    add question validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205163835) do
+ActiveRecord::Schema.define(version: 20171205195141) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20171205163835) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
