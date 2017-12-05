ff091f7318e506282337bf33d01ed9d0fe831937
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-12-05 11:40:24 -0500

    migrate admin change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204215836) do
+ActiveRecord::Schema.define(version: 20171205163835) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171204215836) do
     t.string "location"
     t.string "github_url"
     t.string "previous_industry"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
