5777da2a717871629b0bae3eb66162e263d22da1
natalietate <martin.nat@gmail.com>
2017-12-06 11:35:12 -0500

    create models for tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,6 @@ class Question < ApplicationRecord
   validates :title, :body, presence: true
   belongs_to :user
   scope :by_tag1, -> tag1 { where(:tag1 => tag1) }
+  has_many :taggings
+  has_many :tags, through: :taggings
 end
