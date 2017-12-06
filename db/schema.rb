8c736529bb36d83243e699e00be7da1e45fc4c6b
natalietate <martin.nat@gmail.com>
2017-12-06 11:38:56 -0500

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206031417) do
+ActiveRecord::Schema.define(version: 20171206163249) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -48,6 +48,21 @@ ActiveRecord::Schema.define(version: 20171206031417) do
     t.text "body"
     t.integer "user_id"
     t.string "tag1"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_taggings_on_question_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
