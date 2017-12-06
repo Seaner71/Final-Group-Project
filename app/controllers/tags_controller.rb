20f473cb05b8d6eedd8e09f8949d253f14f9db2a
echo 'hello world <ssmyth1071@gmail.com>
2017-12-06 10:18:42 -0500

    updat default value for select box

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
     @questions = Question.all
-    @question_tag_array = @questions.map {|x| x.tag1}.uniq
+    @question_tag_array = @questions.map {|q| q.tag1}.uniq
   end
 end
