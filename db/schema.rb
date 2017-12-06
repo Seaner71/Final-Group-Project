582484caa377b78151a6b7b74727ab25386de074
OggiDanailov <ogidan@abv.bg>
2017-12-06 15:26:01 -0500

    my first commit; 14 to go

--- db/schema.rb
+++ db/schema.rb
@@ -53,8 +53,6 @@ ActiveRecord::Schema.define(version: 20171206194812) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
