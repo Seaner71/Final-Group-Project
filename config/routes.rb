38605a5a54cc82d01f9edc4f14bb4bfbe6163641
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2017-12-06 10:51:36 -0500

    nest blog resources within users

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
 
-
   get 'tags/index'
-
 
   devise_for :users
   get 'welcome/index'
   post 'users/:id/', to: 'users#show'
 
-
-resources :users
-resources :blogs
-
-
-
+  resources :users do
+    resources :blogs
+end
 
   resources :questions do
       resources :answers do
@@ -22,13 +17,7 @@ resources :blogs
           put "dislike", to: "answers#downvote"
         end
       end
-
   end
-
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
 end
