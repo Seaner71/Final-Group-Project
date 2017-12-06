10859156eb9fd2bb6b635064af725a540288dbb4
natalietate <martin.nat@gmail.com>
2017-12-06 11:52:56 -0500

    render tags on question, add resources

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @questions = Question.all
-    @question_tag_array = @questions.map {|q| q.tag1}.uniq
+
   end
 
   def show
-    @questions = Question.all
-    
+
+
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -7,7 +7,9 @@ class Question < ApplicationRecord
   has_many :tags, through: :taggings
 
   def tag_list
-    tags.join(", ")
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
   end
 
   def tag_list=(tags_string)
