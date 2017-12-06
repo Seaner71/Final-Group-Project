455ca7df7185a11fb1ba87b524331e4db27888f2
echo 'hello world <ssmyth1071@gmail.com>
2017-12-06 17:34:10 -0500

    update search functionality

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,9 +9,9 @@ class Question < ApplicationRecord
 
   #### Justin Weiss filtering stuff
   scope :title, -> (title) { where title: title }
-  scope :contains, -> (body) { where body: ("body like ?", "%#{body}%")}
+  scope :contains, -> (body) { where("body like ?", "%#{body}%")}
   # scope :location, -> (location_id) { where location_id: location_id }
-
+  # scope :starts_with, -> (name) { where("name like ?", "#{name}%")}
    # early of my attempt to search by tag (wrong model???)
   # scope :by_tag, -> (name) { where name: name }
 
