c8923c20112bc3031d30c2bcfb4d3daaae7bda66
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-12-07 16:21:41 -0500

    add end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206222133) do
+ActiveRecord::Schema.define(version: 20171207195412) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
