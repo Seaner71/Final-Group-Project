992612aba8a32b0ec6056e5ebebd2b708838e680
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-12-07 15:06:23 -0500

    add avatars to blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,8 @@ class Blog < ApplicationRecord
   validates :title, :body, presence: true
   has_many :blog_taggings
   has_many :tags, through: :blog_taggings
+  has_attached_file :avatar, styles: { medium: "300x300>", middle: "230x230>", thumb: "100x100>" }, default_url: "/assets/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def tag_list
     self.tags.collect do |tag|
