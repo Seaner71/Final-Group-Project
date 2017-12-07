cf46c112d45f2cda8db23d148569693a0401385a
natalietate <martin.nat@gmail.com>
2017-12-07 12:24:13 -0500

    render tags on page

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206194812) do
+ActiveRecord::Schema.define(version: 20171206222133) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -47,15 +46,6 @@ ActiveRecord::Schema.define(version: 20171206194812) do
     t.datetime "updated_at", null: false
     t.index ["blog_id"], name: "index_blog_taggings_on_blog_id"
     t.index ["tag_id"], name: "index_blog_taggings_on_tag_id"
-  end
-
-  create_table "blog_tags", force: :cascade do |t|
-    t.integer "tag_id"
-    t.integer "blog_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["blog_id"], name: "index_blog_tags_on_blog_id"
-    t.index ["tag_id"], name: "index_blog_tags_on_tag_id"
   end
 
   create_table "blogs", force: :cascade do |t|
