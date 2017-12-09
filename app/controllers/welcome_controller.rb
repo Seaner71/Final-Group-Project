dfa055c1bbd91b9cf9a622ff017bff616f6e6c0f
echo 'hello world <ssmyth1071@gmail.com>
2017-12-09 00:10:13 -0500

    add best questions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,13 @@ class WelcomeController < ApplicationController
     @blogs = Blog.all
     @tag = Tag.new
     @questions = Question.all
+    @answers = Answer.all
     @sort_by_answers = @questions.joins(:answers).group("questions.id").count.sort.reverse
+    @sort =  Answer.find_by_sql("
+      select * from questions
+      inner join answers on questions.id = answers.question_id
+      group by questions.title
+      order by answers.cached_votes_up DESC")
     @blog = Blog.find_by_id(params[:id])
     @user = User.find_by_id(params[:id])
     @question = Question.find_by_id(params[:id])
